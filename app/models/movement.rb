b9f9b2e6106f7dc416a9970a044c31b032d867e7
orozcoding <orozcoding@gmail.com>
2022-05-23 05:41:07 -0400

    Improve validations

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -3,6 +3,9 @@ class Movement < ApplicationRecord
   has_many :group_movements
   has_many :groups, through: :group_movements
 
-  validates :name, length: { maximum: 300 }
+  validates :name, length: { maximum: 300, message: 'The name is too long' }
+  validates :name, presence: { message: 'Please submit a name' }
+  validates :group_ids, presence: { message: 'Please select at least one category' }
+  validates :amount, presence: { message: 'Please submit an amount' }
   validates :amount, numericality: true
 end
